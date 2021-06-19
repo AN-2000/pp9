e1672328fd43ec67366a8196747b099389b2a157
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-06-19 20:33:05 +0530

    update

--- camera/dbScript.js
+++ camera/dbScript.js
@@ -14,12 +14,35 @@ req.addEventListener("error", function () {
   console.log("error");
 });
 
+//script.js
 function addMedia(media, type) {
   if (!db) return;
   let obj = { mId: Date.now(), media, type };
 
-
   let tx = db.transaction("Gallery", "readwrite");
   let gallery = tx.objectStore("Gallery");
   gallery.add(obj);
+}
+
+//gallery.html
+function viewMedia() {
+  let tx = db.transaction("Gallery", "readonly");
+  let gallery = tx.objectStore("Gallery");
+  let cReq = gallery.openCursor();
+
+  cReq.addEventListener("success", function () {
+    let cursor = cReq.result;
+    if (cursor) {
+      let mo = cursor.value;
+
+      //dom ke through media container banao
+      if(mo.type=="video"){
+        //I have to render a video tag
+      }else{
+        //I have to render a image tag
+      }
+
+      cursor.continue();
+    }
+  });
 }
