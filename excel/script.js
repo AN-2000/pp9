96ceecd6c4527114ca0289c64f51a3edd67933c9
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-06-24 19:39:22 +0530

    update

--- excel/script.js
+++ excel/script.js
@@ -1,21 +1,28 @@
 let body = document.querySelector("body");
 body.spellcheck = false;
 
+let columnTags = document.querySelector(".column-tags");
+
 let menuBarPtags = document.querySelectorAll(".menu-bar p");
 
-//
 for (let i = 0; i < menuBarPtags.length; i++) {
   menuBarPtags[i].addEventListener("click", function (e) {
     if (e.currentTarget.classList.contains("menu-bar-option-selected")) {
       e.currentTarget.classList.remove("menu-bar-option-selected");
     } else {
-
-        for(let j = 0;j<menuBarPtags.length;j++){
-            if(menuBarPtags[j].classList.contains("menu-bar-option-selected"))
-            menuBarPtags[j].classList.remove("menu-bar-option-selected")
-        }
+      for (let j = 0; j < menuBarPtags.length; j++) {
+        if (menuBarPtags[j].classList.contains("menu-bar-option-selected"))
+          menuBarPtags[j].classList.remove("menu-bar-option-selected");
+      }
 
       e.currentTarget.classList.add("menu-bar-option-selected");
     }
   });
+}
+
+for (let i = 0; i < 26; i++) {
+  let div = document.createElement("div");
+  div.classList.add("column-tag-cell");
+  div.innerText = String.fromCharCode(65 + i);
+  columnTags.append(div);
 }
