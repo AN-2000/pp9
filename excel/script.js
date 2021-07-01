f8ed79c92f761a4b80a2d1e2dd959c7d2ab204fa
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-01 21:43:19 +0530

    up

--- excel/script.js
+++ excel/script.js
@@ -16,6 +16,8 @@ let menuBarPtags = document.querySelectorAll(".menu-bar p");
 
 let fileOptions = menuBarPtags[0];
 
+let dataObj = {};
+
 fileOptions.addEventListener("click", function (e) {
   if (e.currentTarget.classList.length == 0) {
     e.currentTarget.innerHTML = `File
@@ -24,12 +26,61 @@ fileOptions.addEventListener("click", function (e) {
      <span>Open</span>
      <span>Save</span>
     </span>`;
+
+    let allFileOptions = e.currentTarget.querySelectorAll("span>span");
+
+    //clear
+    allFileOptions[0].addEventListener("click", function () {
+      let allCells = document.querySelectorAll(".cell");
+      for (let i = 0; i < allCells.length; i++) {
+        allCells[i].innerText = "";
+        let cellAdd = allCells[i].getAttribute("data-address");
+        console.log(cellAdd);
+        dataObj[cellAdd] = {
+          value: "",
+          formula: "",
+          upstream: [],
+          downstream: [],
+          fontSize: 10,
+          fontFamily: "Arial",
+          fontWeight: "normal",
+          color: "black",
+          backgroundColor: "white",
+          underline: "none",
+          italics: "normal",
+          textAlign: "left",
+        };
+      }
+    });
+
+    //open
+    allFileOptions[1].addEventListener("click", function () {
+      //1 - Fetch dataObj from localstorage
+      //2 - replace current dataObj with fetched obj
+      dataObj = JSON.parse(localStorage.getItem("sheet"));
+
+      //3 - Populate UI with this data
+
+      for (let j = 1; j <= 100; j++) {
+        for (let i = 0; i < 26; i++) {
+          let address = String.fromCharCode(i + 65) + j;
+          let cellObj = dataObj[address];
+          let cellOnUi = document.querySelector(`[data-address=${address}]`);
+          cellOnUi.innerText = cellObj.value;
+          cellOnUi.style.backgroundColor = cellObj.backgroundColor;
+          //same kaam css styling kelie kr sakta hu?
+        }
+      }
+    });
+
+    //save
+    allFileOptions[2].addEventListener("click", function () {
+      localStorage.setItem("sheet", JSON.stringify(dataObj));
+    });
   } else {
     e.currentTarget.innerHTML = `File`;
   }
 });
-
-let dataObj = {};
 
 for (let i = 0; i < menuBarPtags.length; i++) {
   menuBarPtags[i].addEventListener("click", function (e) {
