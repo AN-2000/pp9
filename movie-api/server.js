ac093a41f8a36033d6cb74ced981d81696e267c0
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-15 20:46:45 +0530

    up

--- movie-api/server.js
+++ movie-api/server.js
@@ -1,18 +1,25 @@
 let express = require("express");
 
+let data = require("./data.json")
+
+
 //ek naya server bnade but ye sirf create krti use chalu nhi krti
 let server = express();
 
+
+
 server.get("/movies", function (req, res) {
-  res.send("movies data from server");
+  res.json(data)
 });
 
 server.get("/genre", function (req, res) {
+
+
     res.send("genre data from server");
   });
 
 //ye line server ko shuru krdeti hai
 // ek port pr
-server.listen(3000);
+server.listen(4000);
 
-//  http://localhost:3000/abc
+// //  http://localhost:3000/movies
