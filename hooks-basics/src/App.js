05a0929135847fcb9857bf0652c4339fe0b26c0f
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-22 21:44:00 +0530

    hooks intro

---
+++ hooks-basics/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function App() {
+  let [count, setCount] = useState(0);
+
+  // console.log(count);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
+}
+
+export default App;
