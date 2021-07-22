c823f4e1cb0a03aef00e8f25bb9926a0478376da
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-22 22:07:03 +0530

    useEffect case 1

--- hooks-basics/src/App.js
+++ hooks-basics/src/App.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App() {
-  let [count, setCount] = useState(0);
 
-  // console.log(count);
+  let [count, setCount] = useState(0); // ye line skip hojati hai
+  
+
+  //case 1: works like componentDidMount
+    useEffect(()=>{
+      console.log("use effect was executed");
+    },[])
+
+
+  console.log("component was rendered");
 
   return (
     <div>
