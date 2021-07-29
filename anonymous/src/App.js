3544859a064226b6d3495d3be0c3a9c5e54a8032
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-29 20:59:15 +0530

    up

--- anonymous/src/App.js
+++ anonymous/src/App.js
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+import { firestore } from "./firebase";
+
 function App() {
+  useEffect(() => {
+    let f = async () => {
+      let querySnapshot = await firestore.collection("posts").get();
+
+
+      console.log(querySnapshot);
+
+      querySnapshot.forEach((doc) => {
+        console.log({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+    };
+
+    f();
+  }, []);
+
   return <div></div>;
 }
 
