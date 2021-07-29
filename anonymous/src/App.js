5787346c215c0489f224a1ab20a86edd89298316
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-29 21:23:32 +0530

    up

--- anonymous/src/App.js
+++ anonymous/src/App.js
@@ -1,26 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { firestore } from "./firebase";
 
 function App() {
+  let [posts, setPosts] = useState([]);
+
   useEffect(() => {
     let f = async () => {
       let querySnapshot = await firestore.collection("posts").get();
 
-
-      console.log(querySnapshot);
+      let tempArr = [];
 
       querySnapshot.forEach((doc) => {
-        console.log({
+        tempArr.push({
           id: doc.id,
           data: doc.data(),
         });
       });
+
+      setPosts(tempArr);
     };
 
     f();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {posts.map((el) => (
+          <li key={el.id}>{el.data.Body}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
