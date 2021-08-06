2c946903f74195986ed4ee20c7f9b3cdaf7db533
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-06 20:55:45 +0530

    up

--- reels/src/VideoCard.jsx
+++ reels/src/VideoCard.jsx
@@ -1,10 +1,49 @@
+import { useState } from "react";
+
 let VideoCard = () => {
+  let [boxOpen, setBoxOpen] = useState(false);
+
   return (
     <div className="video-card">
+      <video src="https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4"></video>
       <span class="material-icons-outlined like">favorite_border</span>
-      <span class="material-icons-outlined comment">chat_bubble</span>
-      <p className="username"></p>
-      <p className="song"></p>
+      <span
+        class="material-icons-outlined comment"
+        onClick={() => {
+          if (boxOpen) setBoxOpen(false);
+          else setBoxOpen(true);
+        }}
+      >
+        chat_bubble
+      </span>
+      <p className="username">
+        <b>@username</b>
+      </p>
+      <p className="song">
+        <span class="material-icons-outlined">music_note</span>
+        <marquee>song name</marquee>
+      </p>
+
+      {boxOpen ? (
+        <div className="comment-box">
+          <button
+            className="comment-box-close-btn"
+            onClick={() => {
+              setBoxOpen(false);
+            }}
+          >
+            Close
+          </button>
+
+          <div className="all-comments"></div>
+          <div className="comment-form">
+            <input />
+            <button>Post</button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
